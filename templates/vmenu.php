9f7d727c5705320c375e4711a4289689c3551be9
Victoria Holland <victoriaholland1984@hotmail.com>
2014-03-09 16:52:18 +0000

    SunsetCity WordPress theme

---
+++ templates/vmenu.php
@@ -0,0 +1,26 @@
+<div class="art-vmenublock <?php echo $class; ?>" <?php echo $id; ?>>
+    <div class="art-vmenublock-tl"></div>
+    <div class="art-vmenublock-tr"></div>
+    <div class="art-vmenublock-bl"></div>
+    <div class="art-vmenublock-br"></div>
+    <div class="art-vmenublock-tc"></div>
+    <div class="art-vmenublock-bc"></div>
+    <div class="art-vmenublock-cl"></div>
+    <div class="art-vmenublock-cr"></div>
+    <div class="art-vmenublock-cc"></div>
+    <div class="art-vmenublock-body">
+<?php echo $header; ?>
+<div class="art-vmenublockcontent">
+    <div class="art-vmenublockcontent-body">
+<!-- block-content -->
+<?php echo $content; ?>
+
+<!-- /block-content -->
+
+		<div class="cleared"></div>
+    </div>
+</div>
+
+		<div class="cleared"></div>
+    </div>
+</div>
